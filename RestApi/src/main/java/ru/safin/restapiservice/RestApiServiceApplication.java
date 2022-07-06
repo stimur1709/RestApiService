fc1964a27a825fefc7eb628a73af366fab6c44d5
stimur1709 <stimur1709@mail.ru>
2022-07-06 10:50:21 +0700

    Создание проекта

---
+++ RestApi/src/main/java/ru/safin/restapiservice/RestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.safin.restapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiServiceApplication.class, args);
+    }
+
+}
