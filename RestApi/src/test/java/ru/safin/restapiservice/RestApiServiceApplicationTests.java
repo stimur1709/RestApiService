e458221991680000b0c2f8141793b077dc8bc921
stimur1709 <stimur1709@mail.ru>
2022-07-06 17:35:36 +0700

    Добавлен readme

---
+++ RestApi/src/test/java/ru/safin/restapiservice/RestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safin.restapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
